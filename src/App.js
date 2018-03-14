0a705bb52be029f63c59db7f324ec87c18deeff2
ysc <bellahuang1012@gmail.com>
2018-03-14 13:37:39 -0400

    SignUpPage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PasswordInput from './DayTwo/PasswordInput';
 import ReusableSelect from './DayTwo/ReusableSelectComponent';
 import BootstrapTest from './DayTwo/BootstrapTest';
 import BirthdateSelector from './DayThree/BirthdateSelector';
+import SignUpPage from './DayThree/SignUpPage';
 // import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -16,20 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App col-xs-12">
-        <h1>Welcome to IOT Weather</h1>
-        <form>
-          <InputComponent labelText="First Name: " placeholder="first name" />
-          <InputComponent labelText="Last Name: " placeholder="last name" />
-          <InputComponent labelText="Email: " placeholder="email@gmail.com" />
-          <InputComponent type="number" labelText="Age: " placeholder="age" />
-          <InputComponent labelText="Birth Day: " placeholder="birth day" />
-          <ReusableSelect label="Birth Month: " optionsArray={[1,2,3,4,5,6,7,8,9,10,11,12]} />
-          <ReusableSelect label="Birth Year: " optionsArray={[2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992, 1991, 1990]} />
-          <BirthdateSelector minYear={1900} />
-          <PasswordInput label="Password: " type="password" placeholder="password (no more than 8 characters)" />
-          <PasswordInput label="Confirm Password: " type="password" placeholder="password (no more than 8 characters)" />
-          <BootstrapTest  />
-        </form>
+        <SignUpPage />
       </div>
     );
   }

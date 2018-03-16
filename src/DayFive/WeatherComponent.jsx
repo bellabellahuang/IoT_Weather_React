0a86ea4d7f890bed2178409407932e19aa1a8960
ysc <bellahuang1012@gmail.com>
2018-03-16 13:53:32 -0400

    display weather infomation

--- src/DayFive/WeatherComponent.jsx
+++ src/DayFive/WeatherComponent.jsx
@@ -6,19 +6,26 @@ export default class WeatherComponent extends Component {
         super();
         this.state = {
             city: props.city,
+            temp: null,
+            description: null,
         }
     }
 
     componentWillMount() {
         
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${api_key}`)
-            .then((response) => {return response.json();}).then((json) => {console.log(json);});
+            .then((response) => {return response.json();}).then((json) => {
+                this.setState({temp: `${Math.round((json.main.temp - 273.15) * 100) / 100}°C`, description: json.weather[0].description });
+                console.log(json);
+            });
     }
 
     render () {
         return (
             <div className="weather-component">
-            
+                <h2>Weather Report For <b><em>{this.state.city}</em></b></h2>
+                <h3>{this.state.temp} - {this.state.description}</h3>
+                
             </div>
         );
     }

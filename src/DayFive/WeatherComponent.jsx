bfc0be61840e63ba7611a354e2196e116f6105c6
ysc <bellahuang1012@gmail.com>
2018-03-19 11:00:05 -0400

    use coords to grab weather

--- src/DayFive/WeatherComponent.jsx
+++ src/DayFive/WeatherComponent.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// import {geolocated} from 'react-geolocated';
 const api_key = '19f2d22ace813941ead56856922e3baf';
 
 export default class WeatherComponent extends Component {
@@ -11,22 +13,54 @@ export default class WeatherComponent extends Component {
         }
     }
 
-    componentWillMount() {
-        
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${api_key}`)
-            .then((response) => {return response.json();}).then((json) => {
-                this.setState({temp: `${Math.round((json.main.temp - 273.15) * 100) / 100}°C`, description: json.weather[0].description });
+    grabWeather(apiCall){
+        fetch(apiCall).then((response) => {return response.json();}).then((json) => {
+                this.setState({city:json.name, temp: `${Math.round((json.main.temp - 273.15) * 100) / 100}°C`, description: json.weather[0].description });
                 console.log(json);
             });
+    }
+
+    componentWillMount() {
+        
+        // fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${api_key}`)
+        //     .then((response) => {return response.json();}).then((json) => {
+        //         this.setState({temp: `${Math.round((json.main.temp - 273.15) * 100) / 100}°C`, description: json.weather[0].description });
+        //         console.log(json);
+        //     });
+
+        if(this.props.city){
+            this.grabWeather(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${api_key}`);
+        }else if(this.props.coords){
+            this.grabWeather(`http://api.openweathermap.org/data/2.5/weather?lat=${this.props.coords.latitude}&lon=${this.props.coords.longitude}&appid=${api_key}`);
+        }else {
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.grabWeather(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${api_key}`);                
+            });
+        }
     }
 
     render () {
         return (
             <div className="weather-component">
-                <h2>Weather Report For <b><em>{this.state.city}</em></b></h2>
-                <h3>{this.state.temp} - {this.state.description}</h3>
+                <h2>Weather Report For <span className="text-danger"><b><em>{this.state.city}</em></b></span></h2>
+                <h3 className="text-primary">{this.state.temp} - {this.state.description}</h3>
                 
             </div>
         );
     }
+}
+
+// export default geolocated({
+//   positionOptions: {
+//     enableHighAccuracy: false,
+//   },
+//   userDecisionTimeout: 5000,
+// })(WeatherComponent);
+
+WeatherComponent.propTypes = {
+    city: PropTypes.string,
+    coords: PropTypes.shape({
+        latitude: PropTypes.number,
+        longitude: PropTypes.number,
+    }),
 }
--- src/DayFive/WeatherPage.jsx
+++ src/DayFive/WeatherPage.jsx
@@ -7,6 +7,8 @@ export default class WeatherPage extends Component {
             <div className="weather-page">
                 <h1>Weather Page</h1>
                 <WeatherComponent city="Sarnia" />
+                <WeatherComponent coords={{latitude:35, longitude:139}} />
+                <WeatherComponent />                
             </div>
         );
     }

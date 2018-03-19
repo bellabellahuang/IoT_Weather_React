f08c4484266307051720ceac421f13ec1d383133
ysc <bellahuang1012@gmail.com>
2018-03-19 11:56:14 -0400

    card list display

--- src/MemoryGame/MemoryGame.js
+++ src/MemoryGame/MemoryGame.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
-import '../App.css';
+import CardItem from './CardItem';
+// import '../App.css';
 
-class App extends Component {
+export default class MemoryGame extends Component {
+
     render() {
+        const cardList = [];
+        for (let i=1; i<=4; i++){
+            for (let j=1; j<=4; j++){
+                cardList.push(<CardItem />);
+            }
+        }
+
         return (
             <div className="App">
                 <h1>Memory Game</h1>
+                <div className="col-xs-12">
+                    {cardList}
+                </div>
             </div>
         );
     }
 }
-
-export default App;
